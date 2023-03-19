2ac9858db0fa4f1c88e0913dbb17e58e1a501fd5
Roman160784 <matveyenkaraman@gmail.com>
2023-03-19 11:49:07 +0300

    remove comment with modal

--- src/Components/Coments/Coment/coment.tsx
+++ src/Components/Coments/Coment/coment.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from '../../../Common/Button/button';
 import { EditableSpan } from '../../../Common/EditableSpan/editableSpan';
 import { Modal } from '../../../Common/Modal/modal';
@@ -12,10 +12,21 @@ type ComentPropsType = {
 
 export const Coment = ({ comment, ...props }: ComentPropsType) => {
 
+    const [modal, setModal] = useState<boolean>(false)
+
     const dispatch = useAppDispatch()
 
-    const removePostHandler = (commentId: string) => {
+    const removePostHandler = () => {
+        setModal(true)
+    }
+
+    const buttonNoHandler = () => {
+        setModal(false)
+    }
+
+    const buttonYesHandler = (commentId: string) => {
         dispatch(removeCommentTC({ commentId }))
+        setModal(false)
     }
 
     return (
@@ -23,19 +34,19 @@ export const Coment = ({ comment, ...props }: ComentPropsType) => {
             <img className={st.avatar} src="https://w7.pngwing.com/pngs/862/646/png-transparent-beard-hipster-male-man-avatars-xmas-giveaway-icon-thumbnail.png" alt="avatar" />
             <span className={st.name}>{comment.commentatorInfo.userLogin}</span>
             <span className={st.date}>{comment.createdAt}</span>
-            <Modal active={true} setActive={undefined} >
+            <Modal active={modal} setActive={undefined} >
                 <div className={st.modal}>
                     <span className={st.modalTitle}>Do you wants to remove comment???</span>
                     <div className={st.modalButtonBlock}>
-                            <button className={st.buttonYes} onClick={() => { removePostHandler(comment.id) }}>{'Yea I do'}</button>
-                            <button className={st.buttonNo} onClick={() => { removePostHandler(comment.id) }}>{'No no no'}</button>
+                            <button className={st.buttonYes} onClick={() => { buttonYesHandler(comment.id) }}>{'Yea I do'}</button>
+                            <button className={st.buttonNo} onClick={buttonNoHandler}>{'No no no'}</button>
                     </div>
                 </div>
             </Modal>
             <div>
                 <EditableSpan title={comment.content} changeTitle={() => { }} />
             </div>
-            <Button disabled={false} title={'Remove comment'} onClick={() => { removePostHandler(comment.id) }} />
+            <Button disabled={false} title={'Remove comment'} onClick={removePostHandler} />
         </div>
     )
 
